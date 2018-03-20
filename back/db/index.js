465fdb31ed8ca8362b0a199ea3e18ee94b258e71
Igor Yovka <igor.yovka@gmail.com>
2018-03-20 17:17:05 +0200

    db change state

--- back/db/index.js
+++ back/db/index.js
@@ -6,59 +6,60 @@ const mongoose = require('mongoose');
 //mongoose.connect('mongodb://localhost/dota2stats');
 //const db = mongoose.connection;
 
-let mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
-
-if (mongoURL === null && process.env.DATABASE_SERVICE_NAME) {
-    let mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-        mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-        mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-        mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-        mongoPassword = process.env[mongoServiceName + '_PASSWORD'],
-        mongoUser = process.env[mongoServiceName + '_USER'];
-
-    console.log(`mongoHost = ${mongoHost}, mongoPort = ${mongoPort}, mongoDatabase = ${mongoDatabase}, mongoPassword = ${mongoPassword}, mongoUser = ${mongoUser} `);
-    if (mongoHost && mongoPort && mongoDatabase) {
-        mongoURLLabel = mongoURL = 'mongodb://';
-        if (mongoUser && mongoPassword) {
-            mongoURL += mongoUser + ':' + mongoPassword + '@';
-        }
-        // Provide UI label that excludes user id and pw
-        mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-        mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-
-    }
-}
-let db = null,
-    dbDetails = new Object();
-
-const initDb = function(callback) {
-    console.log(`InitDB mongoURL ${mongoURL}`);
-    if (mongoURL == null) return;
-
-    mongoose.connect(mongoURL, function(err, conn) {
-        if (err) {
-            callback(err);
-            return;
-        }
-
-        db = conn;
-        dbDetails.databaseName = db.databaseName;
-        dbDetails.url = mongoURLLabel;
-        dbDetails.type = 'MongoDB';
-
-        console.log('Connected to MongoDB at: %s', mongoURL);
-        callback({},{
-            db: db,
-            dbDetails: dbDetails
-        });
-    });
-};
-
 
 //Bind connection to error event (to get notification of connection errors)
 //db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 module.exports.mongo_db = function (callback) {
+
+    let mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+        mongoURLLabel = "";
+
+    if (mongoURL === null && process.env.DATABASE_SERVICE_NAME) {
+        let mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+            mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+            mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+            mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+            mongoPassword = process.env[mongoServiceName + '_PASSWORD'],
+            mongoUser = process.env[mongoServiceName + '_USER'];
+
+        console.log(`mongoHost = ${mongoHost}, mongoPort = ${mongoPort}, mongoDatabase = ${mongoDatabase}, mongoPassword = ${mongoPassword}, mongoUser = ${mongoUser} `);
+        if (mongoHost && mongoPort && mongoDatabase) {
+            mongoURLLabel = mongoURL = 'mongodb://';
+            if (mongoUser && mongoPassword) {
+                mongoURL += mongoUser + ':' + mongoPassword + '@';
+            }
+            // Provide UI label that excludes user id and pw
+            mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+            mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+
+        }
+    }
+    let db = null,
+        dbDetails = new Object();
+
+    const initDb = function(callback) {
+        console.log(`InitDB mongoURL ${mongoURL}`);
+        if (mongoURL == null) return;
+
+        mongoose.connect(mongoURL, function(err, conn) {
+            if (err) {
+                callback(err);
+                return;
+            }
+
+            db = conn;
+            dbDetails.databaseName = db.databaseName;
+            dbDetails.url = mongoURLLabel;
+            dbDetails.type = 'MongoDB';
+
+            console.log('Connected to MongoDB at: %s', mongoURL);
+            callback({},{
+                db: db,
+                dbDetails: dbDetails
+            });
+        });
+    };
+
     initDb(callback);
 };
